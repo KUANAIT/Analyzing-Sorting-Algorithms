f084e664c989d14a88b8ed70e1e0a5dac579d260
KUANAIT <230703@astanait.edu.kz>
2024-05-14 23:50:42 +0500

    a

--- src/DataGenerator.java
+++ src/DataGenerator.java
@@ -4,10 +4,10 @@ import java.util.Random;
 public class DataGenerator {
     public static int[] generateRandomArray(int size) {
         Random random = new Random();
-        int[] array = new int[size];
+        int[] arr = new int[size];
         for (int i = 0; i < size; i++)
-            array[i] = random.nextInt();
-        return array;
+            arr[i] = random.nextInt();
+        return arr;
     }
 
     public static int[] generateSortedArray(int size) {
